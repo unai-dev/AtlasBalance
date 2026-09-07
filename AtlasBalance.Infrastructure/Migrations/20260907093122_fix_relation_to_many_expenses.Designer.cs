680b2f7102ab01d29c0b143aed4eeefe6ffeeb13
Unai Villar <unaidevcoding@gmail.com>
2026-09-07 11:32:12 +0200

    feat: ejecucion de migracion para solucionar relacion mal formada

--- AtlasBalance.Infrastructure/Migrations/20260907093122_fix_relation_to_many_expenses.Designer.cs
+++ AtlasBalance.Infrastructure/Migrations/20260907093122_fix_relation_to_many_expenses.Designer.cs
@@ -4,6 +4,7 @@ using AtlasBalance.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AtlasBalance.Infrastructure.Migrations
 {
     [DbContext(typeof(AtlasDbContext))]
-    partial class AtlasDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260907093122_fix_relation_to_many_expenses")]
+    partial class fix_relation_to_many_expenses
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AtlasBalance.Infrastructure/Migrations/20260907093122_fix_relation_to_many_expenses.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AtlasBalance.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class fix_relation_to_many_expenses : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_payment_methods_payment_methods_PaymentMethodID",
+                table: "payment_methods");
+
+            migrationBuilder.DropIndex(
+                name: "IX_payment_methods_PaymentMethodID",
+                table: "payment_methods");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentMethodID",
+                table: "payment_methods");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PaymentMethodID",
+                table: "payment_methods",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_payment_methods_PaymentMethodID",
+                table: "payment_methods",
+                column: "PaymentMethodID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_payment_methods_payment_methods_PaymentMethodID",
+                table: "payment_methods",
+                column: "PaymentMethodID",
+                principalTable: "payment_methods",
+                principalColumn: "ID");
+        }
+    }
+}
