e6ac6ab334cfb236731be2c4551d69cdaa5da451
Unai Villar <unaidevcoding@gmail.com>
2026-09-07 19:49:27 +0200

    feat: se crea categoryprofile y se registra en program

--- AtlasBalance.API/Program.cs
+++ AtlasBalance.API/Program.cs
@@ -1,20 +1,61 @@
+using AtlasBalance.Application.MappingProfiles;
 using AtlasBalance.Infrastructure;
 
 using Microsoft.EntityFrameworkCore;
 
+/**
+ | * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ |                                          SERVICES
+ | * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ */
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+/**
+ | * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ |                                          CONTROLLERS
+ | * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ */
 builder.Services.AddControllers();
 
+/**
+ | * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ |                                          DB
+ | * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ */
 string cn = builder.Configuration.GetConnectionString("MariaDB")!;
 builder.Services.AddDbContext<AtlasDbContext>(options =>
 {
     options.UseMySql(cn, ServerVersion.AutoDetect(cn));
 });
 
+/**
+ | * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ |                                          AUTO MAPPER
+ | * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ */
+builder.Services.AddAutoMapper(cfg =>
+{
+    cfg.AddProfile<CategoryProfile>();
+});
 
+
+/**
+ | * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ | * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ | * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ | * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ | * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ | * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ | * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ | * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ | * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+
+/**
+ | * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ |                                          APP
+ | * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ */
 var app = builder.Build();
 
 app.UseHttpsRedirection();
---
+++ AtlasBalance.Application/MappingProfiles/CategoryProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using AtlasBalance.Application.DTOs.Category;
+using AtlasBalance.Domain.Models;
+
+using AutoMapper;
+
+namespace AtlasBalance.Application.MappingProfiles;
+
+public class CategoryProfile: Profile
+{
+    public CategoryProfile()
+    {
+        CreateMap<Category, CategoryReadDto>().ReverseMap();
+        CreateMap<Category, CategoryReadWithRelationsDto>().ReverseMap();
+        CreateMap<CategoryCreateDto, Category>().ReverseMap();
+        CreateMap<CategoryUpdateDto, Category>().ReverseMap();
+    }
+}
