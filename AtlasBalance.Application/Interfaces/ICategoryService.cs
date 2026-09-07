9566d67e38e09e01c024d4460946d70512d671f5
Unai Villar <unaidevcoding@gmail.com>
2026-09-07 19:59:01 +0200

    feat: se crea servicio de categorias

--- AtlasBalance.Application/Interfaces/ICategoryService.cs
+++ AtlasBalance.Application/Interfaces/ICategoryService.cs
@@ -1,13 +1,12 @@
 ﻿using AtlasBalance.Application.DTOs.Category;
 
-namespace AtlasBalance.Application.Interfaces
+namespace AtlasBalance.Application.Interfaces;
+
+public interface ICategoryService
 {
-    public interface ICategoryService
-    {
-        Task<CategoryReadDto> Create(CategoryCreateDto dto);
-        Task Delete(int ID);
-        Task<IEnumerable<CategoryReadDto>> GetAll();
-        Task<CategoryReadDto> GetOne(int ID);
-        Task<CategoryReadWithRelationsDto> GetOneWithRelations(int ID);
-    }
+    Task<IEnumerable<CategoryReadDto>> GetAll();
+    Task<CategoryReadDto> GetOne(int ID);
+    Task<CategoryReadWithRelationsDto> GetOneWithRelations(int ID);
+    Task<CategoryReadDto> Create(CategoryCreateDto dto);
+    Task Delete(int ID);
 }
---
+++ AtlasBalance.Application/Services/CategoryService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using AtlasBalance.Application.DTOs.Category;
+using AtlasBalance.Application.Interfaces;
+using AtlasBalance.Domain.Exceptions;
+using AtlasBalance.Domain.Models;
+using AtlasBalance.Infrastructure;
+
+using AutoMapper;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace AtlasBalance.Application.Services;
+
+public class CategoryService : ICategoryService
+{
+    private readonly AtlasDbContext _context;
+    private readonly IMapper _mapper;
+
+    public CategoryService(AtlasDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<CategoryReadDto>> GetAll()
+        => _mapper.Map<IEnumerable<CategoryReadDto>>(await _context.Categories.AsNoTracking().ToListAsync());
+
+    public async Task<CategoryReadWithRelationsDto> GetOneWithRelations(int ID)
+    {
+        var category = await _context.Categories
+            .Include(x => x.Expenses)
+            .FirstOrDefaultAsync(x => x.ID == ID)
+            ?? throw new NotFoundException($"Category with ID {ID} not found.");
+
+        return _mapper.Map<CategoryReadWithRelationsDto>(category);
+    }
+
+    public async Task<CategoryReadDto> GetOne(int ID)
+    {
+        var category = await _context.Categories
+            .FirstOrDefaultAsync(x => x.ID == ID)
+            ?? throw new NotFoundException($"Category with ID {ID} not found.");
+
+        return _mapper.Map<CategoryReadDto>(category);
+    }
+
+    public async Task<CategoryReadDto> Create(CategoryCreateDto dto)
+    {
+        var existingCategory = await _context.Categories.AnyAsync(x => x.Name == dto.Name);
+
+        if (existingCategory)
+            throw new BadRequestException("Category with the same name already exists.");
+
+        var category = _mapper.Map<Category>(dto);
+
+        _context.Categories.Add(category);
+        await _context.SaveChangesAsync();
+
+        return _mapper.Map<CategoryReadDto>(category);
+    }
+
+    public async Task Delete(int ID)
+    {
+        var category = await _context.Categories
+            .FirstOrDefaultAsync(x => x.ID == ID)
+            ?? throw new NotFoundException($"Category with ID {ID} not found.");
+
+        _context.Categories.Remove(category);
+        await _context.SaveChangesAsync();
+    }
+
+}
