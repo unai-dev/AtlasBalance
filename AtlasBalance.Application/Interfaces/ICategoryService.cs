8acfeabb231d7752c23ec96d4f038de3c54d19bd
Unai Villar <unaidevcoding@gmail.com>
2026-09-07 19:58:50 +0200

    feat: se agrega contrato de interfaz para servicio de categorias

---
+++ AtlasBalance.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using AtlasBalance.Application.DTOs.Category;
+
+namespace AtlasBalance.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<CategoryReadDto> Create(CategoryCreateDto dto);
+        Task Delete(int ID);
+        Task<IEnumerable<CategoryReadDto>> GetAll();
+        Task<CategoryReadDto> GetOne(int ID);
+        Task<CategoryReadWithRelationsDto> GetOneWithRelations(int ID);
+    }
+}
