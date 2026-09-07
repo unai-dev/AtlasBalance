3820a61670a5e1b67128ae59f3240249d940aa67
Unai Villar <unaidevcoding@gmail.com>
2026-09-07 19:55:57 +0200

    feat: se agregan excepciones personalizadas

---
+++ AtlasBalance.Domain/Exceptions/NotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace AtlasBalance.Domain.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException() { }
+    public NotFoundException(string message) : base(message) { }
+}
