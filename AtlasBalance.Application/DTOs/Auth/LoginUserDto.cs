b8dcae9bf4ae656bc45a51e0f6b9130ec30aa70f
Unai Villar <unaidevcoding@gmail.com>
2026-09-10 11:20:12 +0200

    feat: auth dtos

---
+++ AtlasBalance.Application/DTOs/Auth/LoginUserDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AtlasBalance.Application.DTOs.Auth;
+
+public class LoginUserDto
+{
+    [Required]
+    [EmailAddress]
+    [StringLength(256)]
+    public string Email { get; set; } = null!;
+
+    [Required]
+    public string Password { get; set; } = null!;
+}
---
+++ AtlasBalance.Application/DTOs/Auth/RegisterUserDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using AtlasBalance.Application.DTOs.User;
+
+namespace AtlasBalance.Application.DTOs.Auth;
+
+public class RegisterUserDto: UserCreateDto
+{
+    [Required]
+    public string Password { get; set; } = null!;
+}
