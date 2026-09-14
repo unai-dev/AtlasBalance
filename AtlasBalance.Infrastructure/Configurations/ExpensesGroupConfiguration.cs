5cb9ed13a4be7e3005321b7aa2db98120a8549be
Unai Villar <unaidevcoding@gmail.com>
2026-09-14 10:15:08 +0200

    feat: se configura entidad grupo de gastos

---
+++ AtlasBalance.Infrastructure/Configurations/ExpensesGroupConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using AtlasBalance.Domain.Models;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AtlasBalance.Infrastructure.Configurations;
+
+public class ExpensesGroupConfiguration: IEntityTypeConfiguration<ExpensesGroup>
+{
+    public void Configure(EntityTypeBuilder<ExpensesGroup> builder)
+    {
+        builder.ToTable("expenses_groups");
+
+        builder.HasKey(x => x.ID);
+
+        builder.Property(x => x.Name)
+            .HasMaxLength(55)
+            .IsRequired();
+
+        builder.Property(x => x.Description)
+            .HasMaxLength(2000)
+            .IsRequired();
+    }
+}
